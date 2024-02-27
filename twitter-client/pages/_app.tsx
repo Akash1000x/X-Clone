774d440d1b533fdf6278faede470aee48ca41181
Akash Kumar Kumawat <kumawatap63@gmail.com>
2024-02-27 06:04:21 +0530

    Authentication added

--- twitter-client/pages/_app.tsx
+++ twitter-client/pages/_app.tsx
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import {Toaster} from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <GoogleOAuthProvider clientId="685778127383-60ctmcmutcmv7ecp59gmon5gt9jmn57p.apps.googleusercontent.com">
+      <Component {...pageProps} />
+      <Toaster />
+    </GoogleOAuthProvider>
+  );
 }
--- twitter-client/pages/index.tsx
+++ twitter-client/pages/index.tsx
@@ -1,4 +1,7 @@
 import Image from "next/image";
+
+import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
+
 import { Inter } from "next/font/google";
 import { BsTwitterX } from "react-icons/bs";
 import { FiSearch } from "react-icons/fi";
@@ -8,7 +11,11 @@ import { PiBookmarkSimple } from "react-icons/pi";
 import { CgMoreO } from "react-icons/cg";
 import { FaRegUser } from "react-icons/fa6";
 import { GoHome } from "react-icons/go";
-import FeedCard from "@/components/FeedCard";
+import FeedCard from "@/Components/FeedCard";
+import { useCallback } from "react";
+import toast from "react-hot-toast";
+import { verifyUserGoogleTokenQuery } from "@/graphql/query/user";
+import { graphqlClient } from "@/clients/api";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -49,10 +56,30 @@ const SidebarMenuItems: TwitterSidebarButton[] = [
 ];
 
 export default function Home() {
+  const handleLoginWithGoogle = useCallback(
+    async (cred: CredentialResponse) => {
+      const googleToken = cred.credential;
+      if (!googleToken) return toast.error("Google token not foune");
+
+      const { verifyGoogleToken } = await graphqlClient.request(
+        verifyUserGoogleTokenQuery,
+        { token: googleToken }
+      );
+
+      toast.success("verified Successfully");
+      console.log(verifyGoogleToken);
+
+      if(verifyGoogleToken){
+        window.localStorage.setItem("_X_token",verifyGoogleToken);
+      }
+    },
+    []
+  );
+
   return (
     <div className="font-[chirp-regular]">
       <div className="grid grid-cols-12 h-screen w-screen overflow-y-scroll ">
-        <div className="col-span-3   pt-1 ml-12 relative"> 
+        <div className="col-span-3 pt-1 ml-12 relative">
           <div className="fixed">
             <div className="text-3xl hover:bg-zinc-900 cursor-pointer w-fit p-3 ml-3 mb-4 rounded-full transition-all duration-75">
               <BsTwitterX />
@@ -83,7 +110,14 @@ export default function Home() {
           <FeedCard />
           <FeedCard />
         </div>
-        <div className="col-span-3  "></div>
+        <div className="col-span-3 p-5 ">
+          <div className="p-5 bg-zinc-900 rounded-lg">
+            <h1 className="text-xl pb-2 font-[chirp-bold] tracking-wider ">
+              New to X?
+            </h1>
+            <GoogleLogin onSuccess={handleLoginWithGoogle} />
+          </div>
+        </div>
       </div>
     </div>
   );
