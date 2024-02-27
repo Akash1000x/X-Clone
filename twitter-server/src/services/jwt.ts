bd243c733dd9e79becfedebf22bf247e74cb4004
Akash Kumar Kumawat <kumawatap63@gmail.com>
2024-02-27 06:00:38 +0530

    setup server

---
+++ twitter-server/src/services/jwt.ts
@@ -0,0 +1,18 @@
+import JWT from "jsonwebtoken";
+import { User } from "@prisma/client";
+
+const JWT_SECRET = "@#%$^&*()_+_)(*&^%$#@!";
+
+class JWTService{
+    public static  generateTokenForUser(user:User){
+        const payload = {
+            id:user?.id,
+            email:user?.email,
+        }
+
+        const token = JWT.sign(payload,JWT_SECRET);
+        return token;
+    }
+}
+
+export default JWTService;
